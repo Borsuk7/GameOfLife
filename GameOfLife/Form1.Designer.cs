3eb553c54b3cf3b82e0c9f4958269477a3215a71
Borsuk7 <51285442+Borsuk7@users.noreply.github.com>
2020-09-06 16:41:07 +0300

    Add project files.

---
+++ GameOfLife/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace GameOfLife
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.BStop = new System.Windows.Forms.Button();
+            this.Bstart = new System.Windows.Forms.Button();
+            this.nudDensity = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudResolution = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDensity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudResolution)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.BStop);
+            this.splitContainer1.Panel1.Controls.Add(this.Bstart);
+            this.splitContainer1.Panel1.Controls.Add(this.nudDensity);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.nudResolution);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(869, 419);
+            this.splitContainer1.SplitterDistance = 279;
+            this.splitContainer1.TabIndex = 0;
+            this.splitContainer1.Text = "splitContainer1";
+            // 
+            // BStop
+            // 
+            this.BStop.Location = new System.Drawing.Point(12, 123);
+            this.BStop.Name = "BStop";
+            this.BStop.Size = new System.Drawing.Size(75, 23);
+            this.BStop.TabIndex = 4;
+            this.BStop.Text = "Stop";
+            this.BStop.UseVisualStyleBackColor = true;
+            this.BStop.Click += new System.EventHandler(this.BStop_Click);
+            // 
+            // Bstart
+            // 
+            this.Bstart.Location = new System.Drawing.Point(12, 94);
+            this.Bstart.Name = "Bstart";
+            this.Bstart.Size = new System.Drawing.Size(75, 23);
+            this.Bstart.TabIndex = 3;
+            this.Bstart.Text = "Start";
+            this.Bstart.UseVisualStyleBackColor = true;
+            this.Bstart.Click += new System.EventHandler(this.Bstart_Click);
+            // 
+            // nudDensity
+            // 
+            this.nudDensity.Location = new System.Drawing.Point(12, 66);
+            this.nudDensity.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.nudDensity.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.nudDensity.Name = "nudDensity";
+            this.nudDensity.Size = new System.Drawing.Size(120, 22);
+            this.nudDensity.TabIndex = 2;
+            this.nudDensity.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nudDensity.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Density";
+            // 
+            // nudResolution
+            // 
+            this.nudResolution.Location = new System.Drawing.Point(12, 25);
+            this.nudResolution.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.nudResolution.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.nudResolution.Name = "nudResolution";
+            this.nudResolution.Size = new System.Drawing.Size(120, 22);
+            this.nudResolution.TabIndex = 2;
+            this.nudResolution.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nudResolution.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Resolution";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(586, 419);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 419);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nudDensity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudResolution)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.NumericUpDown nudResolution;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown nudDensity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Bstart;
+        private System.Windows.Forms.Button BStop;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ GameOfLife/GameEngine.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameOfLife
+{
+    class GameEngine
+    {
+
+        public uint CurrentGeneration { get; private set; }
+        private bool[,] field;
+        private readonly int rows;
+        private readonly int cols;        
+
+        public GameEngine(int rows, int cols, int density)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            field = new bool[cols, rows];
+            var random = new Random();
+
+            for (var x = 0; x < cols; x++)
+            {
+                for (var y = 0; y < rows; y++)
+                {
+                    field[x, y] = random.Next(density) == 0;
+                }
+            }
+        }
+        public void NextGeneration()
+        {
+            var newField = new bool[cols, rows];
+
+            for (var x = 0; x < cols; x++)
+            {
+                for (var y = 0; y < rows; y++)
+                {
+                    var neighboursCount = CountNeighbours(x, y);
+                    var hasLife = field[x, y];
+
+                    if (!hasLife && neighboursCount == 3)
+                        newField[x, y] = true;
+                    else if (hasLife && (neighboursCount < 2 || neighboursCount > 3))
+
+                        newField[x, y] = false;
+                    else
+                        newField[x, y] = field[x, y];
+
+                }
+            }
+            field = newField;
+            CurrentGeneration++;
+
+        }
+        public bool[,] GetCurrentGeneration()
+        {
+            var result = new bool[cols, rows];
+
+            for (var x = 0; x < cols; x++)
+            {
+                for (var y = 0; y < rows; y++)
+                {
+                    result[x, y] = field[x, y];
+                }
+
+            }
+            return result;
+        }
+        private int CountNeighbours(int x, int y)
+        {
+            var count = 0;
+
+            for (var i = -1; i < 2; i++)
+            {
+                for (var j = -1; j < 2; j++)
+                {
+                    var col = (x + i + cols) % cols;
+                    var row = (y + j + rows) % rows;
+
+                    var isSelfChecking = col == x && row == y;
+                    var hasLife = field[col, row];
+
+                    if (hasLife && !isSelfChecking)
+                        count++;
+                }
+
+            }
+            return count;
+        }
+        private bool ValidateCellPosition(int x, int y) => x >= 0 && y >= 0 && x < cols && y < rows;
+        private void UpdateCell(int x, int y, bool state)
+        {
+            if (ValidateCellPosition(x, y))
+                field[x, y] = state;
+        }
+        public void AddCell(int x, int y)
+        {
+            UpdateCell(x, y, true);
+        }
+        public void RemoveCell(int x, int y)
+        {
+            UpdateCell(x, y, false);
+        }
+
+
+
+    }
+}
+
+
